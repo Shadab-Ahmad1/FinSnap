c35b220bb095eec54c2acf36355b8a3f556ac610
Shadab-Ahmad <ashadab124@gmail.com>
2024-11-25 19:37:25 +0500

    add framer motion animation in testimonials and faq's

--- src/sections/landing/components/Testimonials.jsx
+++ src/sections/landing/components/Testimonials.jsx
@@ -3,6 +3,7 @@ import { Box, Typography, IconButton, Avatar, Container } from '@mui/material';
 import { Icon } from '@iconify/react';
 import chevronLeft from '@iconify/icons-material-symbols/chevron-left';
 import chevronRight from '@iconify/icons-material-symbols/chevron-right';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 const testimonials = [
   {
@@ -45,6 +46,13 @@ const testimonials = [
 
 const Testimonials = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const containerRef = React.useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ['start end', 'end start'],
+  });
+
+  const scale = useTransform(scrollYProgress, [0, 0.5, 1], [0.8, 1, 1]);
 
   const handlePrev = () => {
     setCurrentIndex((prevIndex) => (prevIndex - 1 + testimonials.length) % testimonials.length);
@@ -64,117 +72,119 @@ const Testimonials = () => {
         padding: { xs: '16px', sm: '32px' },
       }}
     >
-      <Box
-        position="relative"
-        sx={{
-          height: '550px',
-          borderRadius: '16px',
-          backgroundImage: `url(${background})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          overflow: 'hidden',
-        }}
-      >
-        {/* Overlay */}
+      <motion.div ref={containerRef} style={{ scale }} initial={{ scale: 0.8 }}>
         <Box
+          position="relative"
           sx={{
-            backgroundColor: '#16162980',
-            backgroundImage: 'linear-gradient(#16162900, #16162973 80%)',
-            position: 'absolute',
-            inset: 0,
-          }}
-        />
-
-        <Box
-          textAlign="center"
-          sx={{
-            position: 'relative',
-            zIndex: 1,
-            color: 'white',
+            height: '550px',
+            borderRadius: '16px',
+            backgroundImage: `url(${background})`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            overflow: 'hidden',
           }}
         >
-          <Avatar
-            alt={name}
-            src={avatar}
+          {/* Overlay */}
+          <Box
             sx={{
-              width: 120,
-              height: 120,
-              margin: '0 auto',
-              marginTop: '4rem',
-              marginBottom: 2,
-              border: '4px solid white',
+              backgroundColor: '#16162980',
+              backgroundImage: 'linear-gradient(#16162900, #16162973 80%)',
+              position: 'absolute',
+              inset: 0,
             }}
           />
-          <Typography
-            variant="h4"
-            component="p"
-            gutterBottom
-            sx={{
-              maxWidth: '900px',
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              color: 'hsl(210, 100%, 16%)',
-            }}
-          >
-            &ldquo;{quote}&rdquo;
-          </Typography>
-          <Typography
-            variant="subtitle2"
-            component="p"
-            sx={{ opacity: '.75', marginTop: '16px', fontSize: '1rem' }}
-          >
-            {name}, {title}
-          </Typography>
-        </Box>
 
-        {/* Chevron Buttons */}
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '50%',
-            left: 0,
-            right: 0,
-            transform: 'translateY(-50%)',
-            display: 'flex',
-            justifyContent: 'space-between',
-            padding: '0 40px',
-            zIndex: 1,
-          }}
-        >
-          <IconButton
-            onClick={handlePrev}
+          <Box
+            textAlign="center"
             sx={{
+              position: 'relative',
+              zIndex: 1,
               color: 'white',
-              background: 'transparent',
-              width: '44px',
-              height: '44px',
-              borderRadius: '50%',
-              '&:hover': {
-                background: 'transparent',
-                borderColor: 'white',
-              },
             }}
           >
-            <Icon icon={chevronLeft} width={36} height={36} />
-          </IconButton>
-          <IconButton
-            onClick={handleNext}
+            <Avatar
+              alt={name}
+              src={avatar}
+              sx={{
+                width: 120,
+                height: 120,
+                margin: '0 auto',
+                marginTop: '4rem',
+                marginBottom: 2,
+                border: '4px solid white',
+              }}
+            />
+            <Typography
+              variant="h4"
+              component="p"
+              gutterBottom
+              sx={{
+                maxWidth: '900px',
+                marginLeft: 'auto',
+                marginRight: 'auto',
+                color: 'hsl(210, 100%, 16%)',
+              }}
+            >
+              &ldquo;{quote}&rdquo;
+            </Typography>
+            <Typography
+              variant="subtitle2"
+              component="p"
+              sx={{ opacity: '.75', marginTop: '16px', fontSize: '1rem' }}
+            >
+              {name}, {title}
+            </Typography>
+          </Box>
+
+          {/* Chevron Buttons */}
+          <Box
             sx={{
-              color: 'white',
-              background: 'transparent',
-              width: '44px',
-              height: '44px',
-              borderRadius: '50%',
-              '&:hover': {
-                background: 'transparent',
-                borderColor: 'white',
-              },
+              position: 'absolute',
+              top: '50%',
+              left: 0,
+              right: 0,
+              transform: 'translateY(-50%)',
+              display: 'flex',
+              justifyContent: 'space-between',
+              padding: '0 40px',
+              zIndex: 1,
             }}
           >
-            <Icon icon={chevronRight} width={36} height={36} />
-          </IconButton>
+            <IconButton
+              onClick={handlePrev}
+              sx={{
+                color: 'white',
+                background: 'transparent',
+                width: '44px',
+                height: '44px',
+                borderRadius: '50%',
+                '&:hover': {
+                  background: 'transparent',
+                  borderColor: 'white',
+                },
+              }}
+            >
+              <Icon icon={chevronLeft} width={36} height={36} />
+            </IconButton>
+            <IconButton
+              onClick={handleNext}
+              sx={{
+                color: 'white',
+                background: 'transparent',
+                width: '44px',
+                height: '44px',
+                borderRadius: '50%',
+                '&:hover': {
+                  background: 'transparent',
+                  borderColor: 'white',
+                },
+              }}
+            >
+              <Icon icon={chevronRight} width={36} height={36} />
+            </IconButton>
+          </Box>
         </Box>
-      </Box>
+      </motion.div>
     </Container>
   );
 };
